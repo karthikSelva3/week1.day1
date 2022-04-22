69e92f2503f160320158bab6f8ee6ab6f5259c0e
karthikSelva3 <42270248+karthikSelva3@users.noreply.github.com>
2022-04-22 12:56:18 +0530

    Add files via upload

---
+++ LearnDatatypes.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class LearnDatatypes {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+byte car = 50;
+short carstoragecapacity = 5;
+int carnumber = 925241528;
+long carnumberfulldetails = 956564545785L;
+float mileage= 6756.87898F;
+double fullmileage = 78787878.67678786879797;
+char model = 'A';
+boolean Istherefuel = true;
+}
